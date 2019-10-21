b65e7b87dfe3c28bbe725d9c3904bbd72d06cace
Mattias-R <mattias.r@gmx.at>
2019-10-21 10:16:38 +0200

    done

--- Mattias/myscript.js
+++ Mattias/myscript.js
@@ -6,33 +6,34 @@
  */
 
 // all elements
-// $("*").css("background-color", "yellow");
+ $("*").css("background-color", "yellow");
 
 // all p elements
-
+$("p")
 
 // the p element with an ID of "intro"
-
+$("#intro")
 
 // all elements with a class of "note"
-
+$(".note")
 
 // all DIV elements with a class of note
-
+$("div.note")
 
 // all p elements in the article element
-
+$("article p")
 
 // the last element in any section which is a p tag
-
+$("p:last")
 
 // the first paragraph on the page
-
+$("p:first")
 
 // all p elements which have a link
-
+//$("p [href]").css("background-color","red");
 
 // the second li within nested ul element
-
+//$("ul li:nth-child(2)").css("background-color","red");
 
 // every second and third p element
+$("p:nth-child(-n+2),p:nth-child(3)").css("background-color","red");
