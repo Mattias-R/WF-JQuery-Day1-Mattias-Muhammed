65aef9a38bca87f593ae8bec99785618a6485886
Mo Faraman <mofarman102@gmail.com>
2019-10-21 11:18:04 +0200

    zip

---
+++ manipulating_html_page_exercise_files/myscript.js
@@ -0,0 +1,16 @@
+$("form").submit(function(e) {
+
+	e.preventDefault();
+	
+	
+	
+
+});
+
+var os = 16;
+//$("form").remove();
+		$("article").append (`<div> HI ${os} </div>`+`<div> HI ${os} </div>`
+	+`<div> HI ${os} </div>`);
+		$("#msg").val("search for ....");
+		$("div").addClass("box");
+		$("a").attr("href","www.codefactory.wien");
